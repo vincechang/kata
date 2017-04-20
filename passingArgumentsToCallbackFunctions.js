29dbcf57d44000b72fb182c3b558d913c7bda6bf
Vincent Chang <vincentchang402@gmail.com>
2017-04-20 22:11:52 +0800

    Add closure solution

--- passingArgumentsToCallbackFunctions.js
+++ passingArgumentsToCallbackFunctions.js
@@ -2,6 +2,7 @@
  * Passing Arguments to Callback Functions
  */
 
+// With ES6 binding.
 const B = function (callback, s) {
   console.log(`B: ${s}`);
   setTimeout(callback.bind(this, s), 1000);
@@ -11,4 +12,19 @@ const A = function (str) {
   console.log(`A: ${str}`);
 };
 
-B(A, 'Ha');
+B(A, 'Foo');
+
+// With closure.
+const D = function (callback, s) {
+  console.log(`D: ${s}`);
+  setTimeout(callback(s), 1000);
+};
+
+const C = function (str) {
+  console.log(`Closure: ${str}`);
+  return function () {
+    console.log(`C: ${str}`);
+  };
+};
+
+D(C, 'Bar');
